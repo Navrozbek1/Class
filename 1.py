99effd2cc6fd5ddbcb6b1d7e0c92ef224c64d9df
Navrozbek <github.navrozbek.352@gmail.com>
2026-01-15 16:47:18 +0500

    ...

---
+++ 1.py
@@ -0,0 +1,31 @@
+""" Class lar bilan ishlashimni brinchi kuni ! """
+
+# class Mashina:
+#     def __init__(self, rang, madel, tezlik):
+#         self.rang = rang
+#         self.madel = madel
+#         self.tezlik = tezlik
+#
+# damas = Mashina("Oq", "Damas 1", 160)
+# gentra = Mashina("Qora", "gentra 2", 220)
+# ticko = Mashina("Qizil", "Ticko 1", 120)
+# nexia = Mashina("Oq", "Nexia 2", 180)
+# bmw = Mashina("Kulrang", "BMW M5 F90", 280)
+#
+# print(f"Damasning rangi: {damas.rang}")
+# print(f"Gentraning rangi: {gentra.rang}")
+# print(f"Tickoning rangi: {ticko.rang}")
+# print(f"Nexianing rangi: {nexia.rang}")
+# print(f"BMW ing rangi: {bmw.rang}\n")
+#
+# print(f"Damasning madeli: {damas.madel}")
+# print(f"Gentraning madeli: {gentra.madel}")
+# print(f"Tickoning madeli: {ticko.madel}")
+# print(f"Nexianing madeli: {nexia.madel}")
+# print(f"BMW ning madeli: {bmw.madel}\n")
+#
+# print(f"Damasning tezligi: {damas.tezlik}")
+# print(f"Gentraning tezligi: {gentra.tezlik}")
+# print(f"Tickoning tezligi: {ticko.tezlik}")
+# print(f"Nexianing tezligi: {nexia.tezlik}")
+# print(f"BMW ning tezligi: {bmw.tezlik}")
